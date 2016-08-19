6edddbe926e529e12dd34910fff47f7db18ef91c
Michaël Marinetti <github@marinetti.fr>
2016-08-19 17:46:24 +0200

    add config/facebook

---
+++ config/facebook.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+return [
+    'default' => [
+        'app_id'  => '{application id}',
+        'app_secret' => '{application secret}',
+        'default_graph_version'          => 'v2.6',
+        //'enable_beta_mode'               => false,
+        //'http_client_handler'            => null,
+        //'persistent_data_handler'        => null,
+        //'pseudo_random_string_generator' => null,
+        //'url_detection_handler'          => null,
+    ],
+
+    // use script to get permanent token for a page
+    'page_id'    => '{page id}',
+    'page_token' => '{page token}',
+
+];
+
